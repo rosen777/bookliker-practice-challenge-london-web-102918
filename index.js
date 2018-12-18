e3d6c121d9ed1a2199e9ef25b1f41efe6e1ce15c
rosen777 <rosen.toshev@flatironschool.com>
2018-12-18 14:50:39 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ document.addEventListener("DOMContentLoaded", init);
 
 const apiURL = 'http://localhost:3000/books'
 const bookListEl = document.querySelector('#list')
+const bookListPanelEl = document.querySelector('#list-panel')
+const bookShowPanelEl = document.querySelector('#show-panel')
+
+let state =  {
+  user: {
+    "id": 1,
+    "username": "pouros"
+  },
+  books: []
+}
 
 function fetchBooks() {
   fetch(apiURL)
     .then(response => response.json())
-    .then(renderBooks)
+    .then(data => {
+      state.books = data
+      renderBooks(data)
+    })
 }
 
 function renderBooks(booksArray) {
@@ -16,49 +29,98 @@ function renderBooks(booksArray) {
 
 function renderSingleBook(book) {
   const bookEl = document.createElement('el')
+
   //results.genres[0].name;
   // results.genres.forEach(function(genre) {
   //  console.log(genre.name);
   //  });
-  let userName = book['users'][0]['username']
+  // let userName = book['users'][0]['username']
   let bookUsernames = []
-  for (let i = 0; i < book.users.length; i++) {
-  bookUsernames.push(book['users'][i]['username'])
-  }
+  let bookArr = []
+  // for (let i = 0; i < book.users.length; i++) {
+  //   bookArr = bookUsernames.push(book['users'][i]['username'])
+  // }
 
-  bookEl.innerHTML = `<h2>${book.title}</h2> <p>${book.description}</p> <img src='${book.img_url}'></img> <p class='user-likes'>${bookUsernames}<br><button data-id='${book.id} class='like-btn'>Like</button></p>`
-  bookListEl.append(bookEl)
+  book.users.forEach (function(element) {
+      bookUsernames.push(element.username)
+  })
+
+  bookEl.innerHTML = `<p id='book-title'>${book.title}</p> </p>`
+  bookListPanelEl.append(bookEl)
+  // const bookTitle = document.querySelector('#book-title')
+
+  bookEl.addEventListener('mouseover', () => {
+  showBook()
+})
+
+function showBook (event) {
+bookShowPanelEl.innerHTML = `
+  <p>${book.description}</p>
+  <img src='${book.img_url}'></img>
+  <p class='user-likes'>${bookUsernames.join(', ')}<br>
+  <button data-id='${book.id}' class='like-btn'>Read this book</button>
+`
+
+likeBook()
+
 }
 
-  // send a patch request to server increasing a toy's like count
-  function addClicksToLikes(){
-  document.addEventListener('click', (e) => {
-    // conditionally render the like number
-    if (e.target.className === "like-btn") {
-      likeBook(e.target.dataset.id).then(console.log)
-    }
+}
+
+const likeBook = () => {
+  const readButton = document.querySelector('.like-btn')
+  readButton.addEventListener('click', (e) => {
+        updateBookLikes(e.target.dataset.id, state.user).then(console.log)
   })
 }
 
-function likeBook(bookId, data) {
+const updateBookLikes = (bookId, data) => {
+  let usersToUpdate;
+  state.books.forEach(function(currentBook) {
+    if(currentBook.id === parseInt(bookId)) {
+      currentBook.users.push(data)
+      usersToUpdate = currentBook.users
+    }
+  })
   return fetch(apiURL + `/${bookId}`, {
     method: 'PATCH',
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      username: 'pouros'
-    })
+    body: JSON.stringify({users: usersToUpdate})
   }).then(res => res.json())
+
 }
+
+  // send a patch request to server increasing a toy's like count
+//   function addClicksToLikes(){
+//   document.addEventListener('click', (e) => {
+//     // conditionally render the like number
+//     if (e.target.className === "like-btn") {
+//       likeBook(e.target.dataset.id).then(console.log)
+//     }
+//   })
+// }
+//
+// function likeBook(bookId, data) {
+//   return fetch(apiURL + `/${bookId}`, {
+//     method: 'PATCH',
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({users.username: data})
+//   }).then(res => res.json())
+//
+// }
 
 // function saveNewBook(e) {
 //   e.preventDefault()
 //   console.log(e)
-//   const name = bookName
+
 // }
 
 function init() {
   fetchBooks()
-  addClicksToLikes()
+  // addClicksToLikes()
+
 }
